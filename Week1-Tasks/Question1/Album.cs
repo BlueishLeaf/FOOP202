62152c933d9760d0ca74541745ce7486143c3a73
Killian Kelly <killiank360@gmail.com>
2018-01-25 12:17:38 +0000

    Finished Week 1

--- Week1-Tasks/Question1/Album.cs
+++ Week1-Tasks/Question1/Album.cs
@@ -9,12 +9,17 @@ namespace Question1
     class Album
     {
         public string AlbumName { get; set; }
-        public int ReleaseYear { get; set; }
+        public DateTime ReleaseYear { get; set; }
         public decimal Sales { get; set; }
-
+        public int YearsSinceRelease { get; set; }
+        public Album(DateTime releaseYear)
+        {
+            ReleaseYear = releaseYear;
+            YearsSinceRelease = DateTime.Now.Year - releaseYear.Year;
+        }
         public override string ToString()
         {
-            return $"{AlbumName},{ReleaseYear},{Sales}";
+            return $"{AlbumName},{ReleaseYear.Year},{Sales},{YearsSinceRelease}";
         }
     }
 }
--- Week1-Tasks/Question1/MainWindow.xaml.cs
+++ Week1-Tasks/Question1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,8 @@ namespace Question1
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Band> bandArray = new List<Band>();
+        List<Band> bandList = new List<Band>();
+        string[] comboOptions = { "All","Pop","Rock","Indie"};
         public MainWindow()
         {
             InitializeComponent();
@@ -29,21 +31,31 @@ namespace Question1
         private void OnWindowLoaded(object sender, RoutedEventArgs e)
         {
             Random geneRandom = new Random();
-            Album a1 = new Album(){AlbumName = "Californication",ReleaseYear = geneRandom.Next(1980,2000),Sales = geneRandom.Next(1000000,2000000)};
-            Album a2 = new Album() { AlbumName = "Fragile", ReleaseYear = geneRandom.Next(1980, 2000), Sales = geneRandom.Next(1000000, 2000000) };
-            Album a3 = new Album() { AlbumName = "Ritchie Blackmore's Rainbow", ReleaseYear = geneRandom.Next(1980, 2000), Sales = geneRandom.Next(1000000, 2000000) };
-            Album a4 = new Album() { AlbumName = "Court of the Crimson King", ReleaseYear = geneRandom.Next(1980, 2000), Sales = geneRandom.Next(1000000, 2000000) };
-            Album a5 = new Album() { AlbumName = "The Dark Side of the Moon", ReleaseYear = geneRandom.Next(1980, 2000), Sales = geneRandom.Next(1000000, 2000000) };
-            Album a6 = new Album() { AlbumName = "Love Gun", ReleaseYear = geneRandom.Next(1980, 2000), Sales = geneRandom.Next(1000000, 2000000) };
+            Album a1 = new Album(GetRandomYear(1980, 2000, geneRandom)) {AlbumName = "Californication", Sales = geneRandom.Next(1000000,2000000)};
+            Album a2 = new Album(GetRandomYear(1980, 2000, geneRandom)) { AlbumName = "Fragile", Sales = geneRandom.Next(1000000, 2000000) };
+            Album a3 = new Album(GetRandomYear(1980, 2000, geneRandom)) { AlbumName = "Ritchie Blackmore's Rainbow", Sales = geneRandom.Next(1000000, 2000000) };
+            Album a4 = new Album(GetRandomYear(1980, 2000, geneRandom)) { AlbumName = "Court of the Crimson King", Sales = geneRandom.Next(1000000, 2000000) };
+            Album a5 = new Album(GetRandomYear(1980, 2000, geneRandom)) { AlbumName = "The Dark Side of the Moon", Sales = geneRandom.Next(1000000, 2000000) };
+            Album a6 = new Album(GetRandomYear(1980, 2000, geneRandom)) { AlbumName = "Love Gun", Sales = geneRandom.Next(1000000, 2000000) };
 
-            bandArray.Add(new PopBand() { BandName = "Red Hot Chilli Peppers", YearFormed = "1983", Members = new List<string> { "Flea", "Anthony Kiedis", "Chad Smith", "Josh Klinghoffer" },Albums = new List<Album>{a1}});
-            bandArray.Add(new RockBand() { BandName = "Rainbow", YearFormed = "1975", Members = new List<string> { "Ritchie Blackmore", "Ronnie James Dio", "Joe Lynn Turner" },Albums = new List<Album>{a2}});
-            bandArray.Add(new IndieBand() { BandName = "King Crimson", YearFormed = "1968", Members = new List<string> { "Robert Fripp", "Mel Collins", "Tony Levin", "Pat Mastelotto","Jakko Jakszyk","Bill Rieflin","Jeremy Stacey" }, Albums = new List<Album> { a3 } });
-            bandArray.Add(new RockBand() { BandName = "Yes", YearFormed = "1968", Members = new List<string> { "Steve Howe", "Alan White", "Geoff Downes", "Billy Sherwood","Jon Davison" }, Albums = new List<Album> { a4 } });
-            bandArray.Add(new IndieBand() { BandName = "Pink Floyd", YearFormed = "1965", Members = new List<string> { "David Gilmour", "Roger Waters", "Syd Barrett","Richard Wright","Nick Mason" }, Albums = new List<Album> { a5 } });
-            bandArray.Add(new PopBand() { BandName = "KISS", YearFormed = "1973", Members = new List<string> { "Paul Stanley", "Gene Simmons", "Tommy THayer","Eric Singer"}, Albums = new List<Album> { a6 } });
-            bandArray.Sort();
-            BandListBox.ItemsSource = bandArray;
+            bandList.Add(new PopBand() { BandName = "Red Hot Chilli Peppers", YearFormed = "1983", Members = new List<string> { "Flea", "Anthony Kiedis", "Chad Smith", "Josh Klinghoffer" },Albums = new List<Album>{a1}});
+            bandList.Add(new RockBand() { BandName = "Rainbow", YearFormed = "1975", Members = new List<string> { "Ritchie Blackmore", "Ronnie James Dio", "Joe Lynn Turner" },Albums = new List<Album>{a2}});
+            bandList.Add(new IndieBand() { BandName = "King Crimson", YearFormed = "1968", Members = new List<string> { "Robert Fripp", "Mel Collins", "Tony Levin", "Pat Mastelotto","Jakko Jakszyk","Bill Rieflin","Jeremy Stacey" }, Albums = new List<Album> { a3 } });
+            bandList.Add(new RockBand() { BandName = "Yes", YearFormed = "1968", Members = new List<string> { "Steve Howe", "Alan White", "Geoff Downes", "Billy Sherwood","Jon Davison" }, Albums = new List<Album> { a4 } });
+            bandList.Add(new IndieBand() { BandName = "Pink Floyd", YearFormed = "1965", Members = new List<string> { "David Gilmour", "Roger Waters", "Syd Barrett","Richard Wright","Nick Mason" }, Albums = new List<Album> { a5 } });
+            bandList.Add(new PopBand() { BandName = "KISS", YearFormed = "1973", Members = new List<string> { "Paul Stanley", "Gene Simmons", "Tommy THayer","Eric Singer"}, Albums = new List<Album> { a6 } });
+            bandList.Sort();
+            BandListBox.ItemsSource = bandList;
+            GenreCombo.ItemsSource = comboOptions;
+        }
+
+        private DateTime GetRandomYear(int startYear, int endYear, Random random)
+        {
+            DateTime start = new DateTime(startYear,1,1);
+            DateTime end = new DateTime(endYear, 1, 1);
+            int range = (end - start).Days;
+            DateTime newDate = start.AddDays(random.Next(range));
+            return newDate;
         }
 
         private void BandListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -57,6 +69,68 @@ namespace Question1
                 YearFormedBlock.Text = selectedBand.YearFormed;
                 MembersBlock.Text = String.Join(",",selectedBand.Members);
                 AlbumListBox.ItemsSource = selectedBand.Albums;
+            }
+        }
+
+        private void GenreCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BandListBox.ItemsSource = null;
+            List<Band> filteredList = new List<Band>();
+            switch (GenreCombo.SelectedIndex)
+            {
+                case 0:
+                    BandListBox.ItemsSource = bandList;
+                    break;
+                case 1:
+                    foreach (Band t in bandList)
+                    {
+                        if(t is PopBand)
+                        {
+                            filteredList.Add(t);
+                        }
+                    }
+                    BandListBox.ItemsSource = filteredList;
+                    break;
+                case 2:
+                    foreach (Band t in bandList)
+                    {
+                        if (t is RockBand)
+                        {
+                            filteredList.Add(t);
+                        }
+                    }
+                    BandListBox.ItemsSource = filteredList;
+                    break;
+                case 3:
+                    foreach (Band t in bandList)
+                    {
+                        if (t is IndieBand)
+                        {
+                            filteredList.Add(t);
+                        }
+                    }
+                    BandListBox.ItemsSource = filteredList;
+
+                    break;
+            }
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Band selectedBand = (Band)BandListBox.SelectedItem;
+            using (StreamWriter writer =
+                new StreamWriter("BandInfo.txt"))
+            {
+                writer.WriteLine(selectedBand.BandName);
+                foreach (string t in selectedBand.Members)
+                {
+                    writer.WriteLine(t);
+                }
+                writer.WriteLine(selectedBand.YearFormed);
+                foreach (Album t in selectedBand.Albums)
+                {
+                    writer.WriteLine(t.AlbumName);
+                }
             }
         }
     }
