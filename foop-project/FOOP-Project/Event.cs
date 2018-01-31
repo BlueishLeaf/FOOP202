a28d9bc3d5a2b54f87d303715f866b89ad4cfb19
Blueishleaf <killiank360@gmail.com>
2018-01-31 13:24:27 +0000

    Project start!

---
+++ foop-project/FOOP-Project/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP_Project
+{
+    class Event
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ foop-project/FOOP-Project/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP_Project
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public Event[] Events { get; set; }
+        public Gift[] Gifts { get; set; }
+    }
+}
