80b18a0f8a5f44349229f959127e06e734d02d23
killian <killiank360@gmail.com>
2018-02-05 20:04:43 +0000

    Finished week 2

---
+++ week2-tasks/Question2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace Question2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Player> playerList = new List<Player>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddPlayerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerBox.ItemsSource = null;
+            playerList.Add(new Player(PlayerNameBox.Text, DOBPicker.SelectedDate.Value));
+            PlayerBox.ItemsSource = playerList;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("players.json"))
+            {
+                writer.Write(JsonConvert.SerializeObject(playerList, Formatting.Indented));
+            }
+        }
+
+        private void LoadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerBox.ItemsSource = null;
+            playerList.Clear();
+            using (StreamReader reader = new StreamReader("players.json"))
+            {
+                List<Player> players = JsonConvert.DeserializeObject<List<Player>>(reader.ReadToEnd());
+                foreach (Player player in players)
+                {
+                    playerList.Add(player);
+                }
+            }
+            PlayerBox.ItemsSource = playerList;
+        }
+    }
+}
---
+++ week2-tasks/Question2/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string Category { get; set; }
+        public Player(string name, DateTime? dob)
+        {
+            Name = name;
+            DOB = dob.Value;
+            int age = DateTime.Now.Year - DOB.Year;
+            if (age <= 10)
+            {
+                Category = "(UNDER 10)";
+            }
+            else if (age <= 12)
+            {
+                Category = "(UNDER 12)";
+            }
+            else if (age <= 14)
+            {
+                Category = "(UNDER 14)";
+            }
+            else if (age <= 16)
+            {
+                Category = "(UNDER 16)";
+            }
+            else if (age < 18)
+            {
+                Category = "(UNDER 18)";
+            }
+            else if (age == 18)
+            {
+                Category = "Senior";
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}",Name,DOB.ToShortDateString(),Category);
+        }
+    }
+}
