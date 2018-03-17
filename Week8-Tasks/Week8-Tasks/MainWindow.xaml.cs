66b5e7d298022e6b39444c11c79239b6199316c6
killian <killiank360@gmail.com>
2018-03-17 01:11:51 +0000

    Started week 8

---
+++ Week8-Tasks/Week8-Tasks/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week8_Tasks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Program _appInstance = new Program();
+        public MainWindow()
+        {
+            InitializeComponent();
+            StockLevelBox.ItemsSource = Enum.GetNames(typeof(Program.StockLevel));
+            SuppliersBox.ItemsSource = _appInstance.GetSuppliers();
+            CountryBox.ItemsSource = _appInstance.GetCountries();
+            ProductsBox.ItemsSource = _appInstance.GetProducts();
+        }
+
+        private void StockLevelBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedOption = StockLevelBox.SelectedItem;
+
+        }
+
+        private void SuppliersBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CountryBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Week8-Tasks/Week8-Tasks/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8_Tasks
+{
+    class Program
+    {
+        public enum StockLevel { Low, Normal, Overstocked }
+        private NORTHWNDEntities _db = new NORTHWNDEntities();
+
+        public IEnumerable GetSuppliers() => _db.Suppliers.Select(s => s.CompanyName).ToList();
+
+        public IEnumerable GetCountries() => _db.Suppliers.Select(s => s.Country).ToList();
+
+        public IEnumerable GetProducts() => _db.Products.Select(p => p.ProductName).ToList();
+    }
+}
